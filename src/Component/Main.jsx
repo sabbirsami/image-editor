a11dfafa4631fa90c7cef60d76997c7e2a0de63d
SabbirSami <smd71430@gmail.com>
2022-08-08 13:24:58 +0600

    make array for filter button

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,9 +1,52 @@
 import React from "react";
+import "./style/main.scss";
 
 const Main = () => {
+    const filterElement = [
+        {
+            name: "brightness",
+        },
+        {
+            name: "grayscale",
+        },
+        {
+            name: "sepia",
+        },
+        {
+            name: "saturate",
+        },
+        {
+            name: "contrast",
+        },
+        {
+            name: "hueRotate",
+        },
+    ];
     return (
         <div>
-            <h1>Main</h1>
+            <div className="image_editor">
+                <div className="card">
+                    <div className="card_header">
+                        <h2>Image Editor</h2>
+                    </div>
+                    <div className="card_body">
+                        <div className="sidebar">
+                            <div className="side_body">
+                                <div className="filter_section">
+                                    <p>Filters</p>
+                                    <div className="filter_key">
+                                        {filterElement.map((v, index) => (
+                                            <button key={index}>
+                                                {v.name}
+                                            </button>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
